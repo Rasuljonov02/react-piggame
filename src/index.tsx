034efc683675b68b4e79c053b49f6612143b0b78
Muhammad <Muhammad@mx>
2024-01-08 15:36:35 +0500

    d

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
-import { createRoot } from 'react-dom/client';
-import Example3 from 'examples/example-3';
+import { createRoot } from "react-dom/client";
 
-import 'assets/style.css';
-import Example2 from 'examples/example-2';
+import Main from "./components/index";
+import "assets/style.css";
 
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById("root")!);
 
 root.render(
-  <>
-    <Example2 />
-    <Example3 />
-  </>
+	<>
+		<Main />
+	</>
 );
